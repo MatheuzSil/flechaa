865c4d9a23545b83a622430b6ea36912144613f9
Theuz Dev <matheuzinhuss44@gmail.com>
2025-03-19 16:23:38 -0300

    .

--- apps/flecha/next.config.js
+++ apps/flecha/next.config.js
@@ -1,33 +1,19 @@
-//@ts-check
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
 
-/**
- * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
- **/
 const nextConfig = {
   nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-
   compiler: {
-    // For other options, see https://styled-components.com/docs/tooling#babel-plugin
     styledComponents: true,
   },
   env: {
     APPLICATION: process.env.APPLICATION || 'Flecha',
     THEME: process.env.THEME || 'flecha',
   },
-  // Adicione esta linha para garantir que o Next.js reconheça o diretório correto
-  basePath: '/apps/flecha',
+  // Adicione esta linha para informar ao Next.js que a pasta `app` está em `src/app`
+  dir: './src',
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
+const plugins = [withNx];
 module.exports = composePlugins(...plugins)(nextConfig);
