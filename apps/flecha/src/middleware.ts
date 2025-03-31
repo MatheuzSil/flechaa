44e5d37090d2ba1aecc4ccf1f63d9996ca4e41cb
Theuz Dev <matheuzinhuss44@gmail.com>
2025-03-31 17:53:00 -0300

    ,

--- apps/flecha/src/middleware.ts
+++ apps/flecha/src/middleware.ts
@@ -1,34 +1,43 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { getToken } from 'next-auth/jwt'; // Importa a função getToken do NextAuth
+import { verifyToken } from '../src/utils/auth'; // Função para verificar o token
 
 // Lista de rotas que não precisam de autenticação
 const publicRoutes = ['/login', '/cadastrar'];
-
-const secret = process.env.AUTH_SECRET;
 
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   // Verifica se a rota atual não é uma rota pública
   if (!publicRoutes.includes(pathname)) {
-    // Obtém o token do NextAuth
-    const token = await getToken({ req, secret });
+    const token = req.cookies.get('token')?.value;
 
     if (!token) {
-      // Redireciona para a página de login se o token não existir ou for inválido
+      // Redireciona para a página de login se o token não existir
       return NextResponse.redirect(new URL('/login', req.url));
     }
 
-    // Adiciona o userId ao cabeçalho da requisição para uso posterior (opcional)
-    const requestHeaders = new Headers(req.headers);
-    requestHeaders.set('userId', token.sub || ''); // `sub` geralmente contém o ID do usuário
+    try {
+      // Verifica se o token é válido
+      const decoded = await verifyToken(token);
+      if (!decoded) {
+        // Redireciona para a página de login se o token for inválido
+        return NextResponse.redirect(new URL('/login', req.url));
+      }
 
-    return NextResponse.next({
-      request: {
-        headers: requestHeaders,
-      },
-    });
+      // Adiciona o userId ao cabeçalho da requisição para uso posterior
+      const requestHeaders = new Headers(req.headers);
+      requestHeaders.set('userId', decoded.userId);
+
+      return NextResponse.next({
+        request: {
+          headers: requestHeaders,
+        },
+      });
+    } catch (error) {
+      console.error('Token verification failed:', error);
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
   }
 
   // Se for uma rota pública, permite a requisição continuar
