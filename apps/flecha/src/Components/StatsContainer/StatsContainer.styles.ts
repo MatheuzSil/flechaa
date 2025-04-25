8260fdc53ac9a00d74302ae723d792a4dcdef4d4
Theuz Dev <matheuzinhuss44@gmail.com>
2025-04-25 14:37:11 -0300

    <update>(StatsList) layout finished

--- apps/flecha/src/Components/StatsContainer/StatsContainer.styles.ts
+++ apps/flecha/src/Components/StatsContainer/StatsContainer.styles.ts
@@ -1,3 +1,4 @@
+import { Paragraph, Title } from '@meu-workspace/safira';
 import styled from 'styled-components';
 
 export const StatsListTitle = styled.h1`
@@ -14,3 +15,58 @@ export const StatsContainer = styled.div`
   align-items: center;
   width: 100%;
 `;
+
+
+export const StatsListBoxsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  width: 100%;
+  margin-top: 20px;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    gap: 10px;
+    margin-top: 10px;
+  }
+`;
+
+export const StatsListBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 700px;
+  height: 72px;
+  background-color: ${({ theme }) => theme.brandColors.primary950};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  padding: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+`
+
+export const StatsTitleAndIconContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
+`
+
+export const StatsListBoxIcon = styled.div``
+
+
+export const StatsListBoxTitle = styled(Title)`
+  font-family: ${({ theme }) => theme.fonts.poppins};
+  font-size: 16px;
+  color: ${({ theme }) => theme.brandColors.primary50};
+  font-weight: 300;
+`
+
+export const StatsListBoxValue = styled(Paragraph)`
+  font-family: ${({ theme }) => theme.fonts.poppins};
+  font-size: 16px;
+  color: ${({ theme }) => theme.brandColors.primary400};
+  font-weight: bold;
+`
--- apps/flecha/src/Components/StatsContainer/StatsContainer.tsx
+++ apps/flecha/src/Components/StatsContainer/StatsContainer.tsx
@@ -1,8 +1,53 @@
+import { BuildingIcon, HeartIcon, PersonIcon, PersonsIcon, SmileIcon } from 'apps/flecha/public/icons/icon';
 import * as S from './StatsContainer.styles';
+
+
 export default function StatsContainer() {
+
+  const stats = [
+    {
+      title: 'Total de crianças cadastradas',
+      value: 1000,
+      icon: <PersonIcon />
+    },
+    {
+      title: 'Total de turmas/salas:',
+      value: 10,
+      icon: <BuildingIcon />
+    },
+    {
+      title: 'Total de responsáveis cadastrados',
+      value: 500,
+      icon: <PersonsIcon />
+    },
+    {
+      title: 'Total de crianças com condições de saude',
+      value: 120,
+      icon: <HeartIcon />
+    },
+    {
+      title: 'Total de crianças especiais',
+      value: 20,
+      icon: <SmileIcon />
+    },
+    
+
+  ]
+
   return (
     <S.StatsContainer>
       <S.StatsListTitle>Plataforma Estatísticas:</S.StatsListTitle>
+      <S.StatsListBoxsContainer>
+        {stats.map((stat, index) => (
+          <S.StatsListBox key={index}>
+            <S.StatsTitleAndIconContainer>
+              <S.StatsListBoxIcon>{stat.icon}</S.StatsListBoxIcon>
+              <S.StatsListBoxTitle>{stat.title}</S.StatsListBoxTitle>
+            </S.StatsTitleAndIconContainer>
+            <S.StatsListBoxValue>{stat.value} (#)</S.StatsListBoxValue>
+          </S.StatsListBox>
+        ))}
+      </S.StatsListBoxsContainer>
     </S.StatsContainer>
   );
 }
