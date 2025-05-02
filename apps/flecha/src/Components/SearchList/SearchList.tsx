86f0b335d769af4f2424e2f7d427cb5b119996a7
Theuz Dev <matheuzinhuss44@gmail.com>
2025-05-02 15:00:43 -0300

    <update>(LoadingPagination)

--- apps/flecha/src/Components/SearchList/SearchList.tsx
+++ apps/flecha/src/Components/SearchList/SearchList.tsx
@@ -3,12 +3,13 @@ import * as S from './SearchList.styles';
 import Pagination from '../Pagination/Pagination';
 import { useEffect, useState } from 'react';
 import { useSearch } from '../../graphql/hooks/useSearch';
+import { Paragraph } from '@meu-workspace/safira';
 
 export default function SearchList() {
   const [query, setQuery] = useState('');
   const [page, setPage] = useState(1);
   const limit = 5;
-  const { results, totalCount, search } = useSearch();
+  const { results, totalCount, search, loading } = useSearch();
   const [paginationTotal, setPaginationTotal] = useState(0);
 
   useEffect(() => {
@@ -38,6 +39,7 @@ export default function SearchList() {
           </S.FiltroSelectContainer>
         </S.FiltrosContainer>
         <S.SearchResultContainer>
+          {loading && <Paragraph $fontColor="#fff">Carregando...</Paragraph>}
           <SearchListResult results={results} />
           <Pagination paginationTotal={paginationTotal} currentPage={page} onPageChange={(newPage) => setPage(newPage)} />
         </S.SearchResultContainer>
