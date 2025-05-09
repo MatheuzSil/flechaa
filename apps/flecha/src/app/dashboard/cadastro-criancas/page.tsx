50888a2d6ca97c0441ce23efe64d3c53f563b01e
Theuz Dev <matheuzinhuss44@gmail.com>
2025-05-09 15:18:49 -0300

    <ChildRegisterPage>

---
+++ apps/flecha/src/app/dashboard/cadastro-criancas/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { ChildRegister } from "../../../Containers/ChildRegister/ChildrRegister";
+
+
+export default function Page() {
+
+  return(
+    <>
+      <ChildRegister />
+    </> 
+   )
+  
+
+}
