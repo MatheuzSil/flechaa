a5eb328f67fb83bdc4c0dc787f62b06144e4fbc0
Theuz Dev <matheuzinhuss44@gmail.com>
2025-05-10 11:58:50 -0300

    <update>(FooterChildForm)

--- apps/flecha/src/Containers/ChildRegister/ChildrRegister.tsx
+++ apps/flecha/src/Containers/ChildRegister/ChildrRegister.tsx
@@ -1,6 +1,7 @@
 import { Title } from "@meu-workspace/safira"
 import { Header } from "../../Components/Header/Header"
 import { ChildFormRegister } from "../../Components/RegisterForms/ChildFormRegister/ChildFormRegister"
+import DashboardFooter from "../../Components/DashboardFooter/DashboardFooter"
 
 
 export const ChildRegister = () => {
@@ -9,6 +10,8 @@ export const ChildRegister = () => {
     <>
       <Header />
       <ChildFormRegister />
+      <DashboardFooter />
+      
     </>
   )
 
